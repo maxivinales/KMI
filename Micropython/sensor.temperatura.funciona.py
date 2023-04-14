46523e12b078c9b83f9224e3479c4f40c2e1d744
Maximiliano <trabajo.facultad47@gmail.com>
2023-04-14 11:31:39 -0300

    Se agrego un ejemplo del sensor de temperatura dallas funcionando

---
+++ Micropython/sensor.temperatura.funciona.py
@@ -0,0 +1,18 @@
+# Escribe tu código aquí :-)
+# Complete project details at https://RandomNerdTutorials.com
+
+import machine, onewire, ds18x20, time
+
+ds_pin = machine.Pin(19)
+ds_sensor = ds18x20.DS18X20(onewire.OneWire(ds_pin))
+
+roms = ds_sensor.scan()
+print('Found DS devices: ', roms)
+
+while True:
+  ds_sensor.convert_temp()
+  time.sleep_ms(750)
+  for rom in roms:
+    print(rom)
+    print(ds_sensor.read_temp(rom))
+  time.sleep(5)
